477ea854f30e27ef3ea233c79eadbf9d19fa134e
Dan <daniel.speranskiy@live.com>
2023-11-29 23:29:08 +0400

    Styled homepage items  need to create share/download btn for img

--- app/page.tsx
+++ app/page.tsx
@@ -9,11 +9,17 @@ import { useRef, useState } from "react"
 import { useForm, SubmitHandler } from "react-hook-form"
 import { ReactSketchCanvas, ReactSketchCanvasRef } from "react-sketch-canvas"
 import { api } from "../convex/_generated/api";
+import {Trash2} from "lucide-react"
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "../components/ui/dialog";
+
+
+
 
 
 
 export default function Home() {
  const [artsIds, setArtsIds] = useState('')
+ const [promptValue, setPromptValue] = useState('')
 const saveArtMutation = useMutation(api.art.uploadArt) 
 //@ts-ignore
 const artsQuery = useQuery(api.art.getArts,{artsIds})
@@ -30,60 +36,76 @@ const canvasRef = useRef<ReactSketchCanvasRef>(null)
    })
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between pt-8">
-      <div className="container mx-auto flex gap-4">
+    <main className="flex min-h-screen flex-col items-center justify-between pt-2 my-4 md:flex-row">
+      <div className="container mx-auto flex flex-col flex-wrap items-center ">
         <form
-          className="flex flex-col gap-2 w-1/4"
+          className="flex flex-col gap-2 "
           onSubmit={handleSubmit(async (formData) => {
             if (!canvasRef.current) return;
             const image = await canvasRef.current.exportImage("jpeg");
             const result = await saveArtMutation({ ...formData, image });
             //@ts-ignore
             setArtsIds(result.id)
-
           })}
         >
-          <Label htmlFor="prompt">Prompt</Label>
-          <Input autoComplete="off" id="prompt" {...register("prompt", { required: true })} />
-          {errors.prompt && <span>This field is required</span>}
-
-          <Label className="mt-4">Canvas (Draw something below)</Label>
-          <ReactSketchCanvas
-            ref={canvasRef}
-            style={{ width:512, height: 512 }}
-            strokeWidth={4}
-            strokeColor="black"
-          />
-{artsQuery && artsQuery.length > 0 && artsQuery[0].result && <img src={artsQuery[0].result} />}
-          
-
+          <Label className="mt-4 text2 text-3xl ">Doodle it !</Label>
+            <ReactSketchCanvas
+              ref={canvasRef}
+              width="40vh"
+              height="40vh"
+              // style={{ width:500, height: '100%', borderRadius: 10 }}
+              strokeWidth={4}
+              strokeColor="black"
+              className="w-1/2"
+              allowOnlyPointerType='all'
+              />
           <Button
             type="button"
-            variant={"ghost"}
+            variant={"outline"}
             onClick={() => {
               canvasRef.current?.clearCanvas();
             }}
+            className="text1 font-medium"
           >
-            Clear
+            <Trash2 />
           </Button>
-
-          <Button type="submit">Submit</Button>
+          <Label htmlFor="prompt" className="text2 text-3xl">Describe it</Label>
+          <Input autoComplete="off" id="prompt" {...register("prompt", { required: true })} onChange={(e) => setPromptValue(e.target.value)} className="w-full text1 font-medium  flex-1" />
+          {errors.prompt && <span>This field is required</span>}
+        
+          <Button type="submit" className="text2 font-bold text-3xl">Submit it!</Button>
         </form>
+   
 
-        <section >
-          {/* <div className="grid grid-cols-4 gap-4">
-          <h2 className="">Recent Sketches</h2>
-            {filteredArts.map((art) => (
-              <img
-                key={art._id}
-                width="256"
-                height="256"
-                src={art.result}
-              />
-            ))}
-          </div> */}
-        </section>
-      </div>
+
+        <div className="mt-5 ">
+         
+        <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Show me</Button>
+      </DialogTrigger>
+      <DialogContent className="rounded-xl ">
+        <DialogHeader className="mt-2 text-center justify-center items-center">
+          <DialogTitle>Here is your amazing art!</DialogTitle>
+          <DialogDescription >
+          {promptValue}
+          </DialogDescription>
+        </DialogHeader>
+        {artsQuery && artsQuery.length > 0 && artsQuery[0].result && (
+      <img src={artsQuery[0].result} className="max-w-full h-auto rounded-xl" alt="Art result" />
+    )}
+        <DialogFooter className="sm:justify-start">
+          <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              Close
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+    
+    </div>
+</div>
     </main>
   )
 }
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,14 +4,13 @@ import Link from 'next/link'
 
 const Nav = () => {
   return (
-    <nav className="border-b py-4">
+    <nav className="border-b py-6 shadow-xl sticky top-0 bg-primary-foreground">
       <div className="container mx-auto flex justify-between">
         <div>LOGO</div>
         <nav className="flex gap-4">
           <Link href="/">Generate</Link>
           <Link href="/collection">Collection</Link>
         </nav>
-      
       </div>
     </nav>
   )
